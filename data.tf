3aec148e99ae04c1d0229cd1acdf4cd9ef688ef4
Major Hayden <major@redhat.com>
2020-08-10 16:30:00 -0500

    Fix remote backend

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-data "terraform_remote_state" "vpc" {
+data "terraform_remote_state" "terraform_cloud" {
   backend = "remote"
   config = {
     organization = "imagebuilder"
--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
   required_version = ">= 0.12"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "imagebuilder"
+    workspaces {
+      name = "imagebuilder-deployment"
+    }
+  }
 }
 
