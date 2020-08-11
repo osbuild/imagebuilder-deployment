76a43b8b4635e71359c71e4fb5d95bc8aa336a2e
Major Hayden <major@redhat.com>
2020-08-11 16:34:08 -0500

    Fix worker>composer communication (#8)
    Signed-off-by: Major Hayden <major@redhat.com>

--- aws_route53.tf
+++ aws_route53.tf
@@ -19,6 +19,6 @@ resource "aws_route53_record" "composer" {
   name            = "composer.${var.deployment_name}.${var.aws_region}.imagebuilder.internal"
   type            = "A"
   ttl             = "300"
-  records         = [aws_eip.composer.public_ip]
+  records         = [aws_instance.composer.private_ip]
   allow_overwrite = true
 }
